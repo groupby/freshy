e5eeead9710f78a868253c8f0af363ad2cecdfad
Samuel Wu <samwwwww@hotmail.com>
2017-08-25 09:38:10 -0400

    WIS-733 Update gb-watchdog to not use decache (#2)

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ var deletedModules = [];
  */
 function unload(module) {
     var path = require.resolve(module);
-    var temp = deletedModules;
+    var temp = deletedModules.slice();
     if (require.cache[path] && require.cache[path].children) {
         require.cache[path].children.forEach(function (child) {
             if(!deletedModules.includes(child.id)) {
@@ -82,12 +82,14 @@ function snapshot(module) {
 var restoredModules = [];
 function restore(module) {
     require.cache[module.id] = module;
+    var temp = restoredModules.slice();
     module.children.forEach(function (child) {
       if(!restoredModules.includes(child.id)){
           restoredModules.push(child.id);
           restore(child);
       }
     });
+    restoredModules = temp;
 }
 
 function normalize(fn) {
